05f446fdb899980fa19422669aacb29e9da9c7af
Ray3163 <hsieh.ray0215@gmail.com>
2024-11-01 11:54:09 +0800

    11/1 Updated

---
+++ Tokyo/data.js
@@ -0,0 +1,362 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-shibuya--outside",
+      "name": "Shibuya  Outside",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.066344517375697,
+        "pitch": -0.02248304343548213,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8552766927910227,
+          "pitch": 0.1599810489246707,
+          "rotation": 0,
+          "target": "1-shibuya-mewtwo"
+        },
+        {
+          "yaw": -2.8617241521805763,
+          "pitch": 0.488316321386268,
+          "rotation": 0,
+          "target": "4-tokyo-dx-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.5351646787247208,
+          "pitch": -0.271839506295354,
+          "title": "澀谷滑板皮卡丘",
+          "text": "為了澀谷店特別繪製的皮卡丘，張貼在澀谷Parco的外牆"
+        }
+      ]
+    },
+    {
+      "id": "1-shibuya-mewtwo",
+      "name": "Shibuya Mewtwo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.12110447870981744,
+        "pitch": -0.09922617930637401,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.09091849016569675,
+          "pitch": 0.24737064609433723,
+          "rotation": 0,
+          "target": "2-shibuya-design-lab"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4297499566311842,
+          "pitch": -0.06510934976775218,
+          "title": "超夢",
+          "text": "依照寶可夢第一部劇場版做出的場景，是我最喜歡的鎮店寶可夢之一"
+        }
+      ]
+    },
+    {
+      "id": "2-shibuya-design-lab",
+      "name": "Shibuya Design lab",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.4395639644162195,
+        "pitch": 0.3338085725142985,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.974494013207595,
+          "pitch": 0.5738747343691806,
+          "rotation": 0,
+          "target": "3-shibuya-inside"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.242522641167465,
+          "pitch": 0.5428353941549169,
+          "title": "Pokemon Design Lab",
+          "text": "澀谷店的主打特色之一，可以將自己喜歡的寶可夢設計成衣服，讓每個人都有屬於自己的寶可衣"
+        }
+      ]
+    },
+    {
+      "id": "3-shibuya-inside",
+      "name": "Shibuya Inside",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.936849088122246,
+        "pitch": 0.3087267255343491,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2436218788052464,
+          "pitch": 0.4694965040939074,
+          "rotation": 0,
+          "target": "2-shibuya-design-lab"
+        },
+        {
+          "yaw": -2.9770945840487197,
+          "pitch": 0.4767624343415111,
+          "rotation": 0.7853981633974483,
+          "target": "1-shibuya-mewtwo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7679613043353068,
+          "pitch": 0.35209162661619864,
+          "title": "Design Lab",
+          "text": "Design Lab 的實際操作地點，與觸控螢幕互動後等待就會產出自己專屬的T-shirt嘍"
+        }
+      ]
+    },
+    {
+      "id": "4-tokyo-dx-entrance",
+      "name": "Tokyo Dx Entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.2894065086162243,
+        "pitch": -0.07378805340547423,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03812994636918354,
+          "pitch": 0.35041806929931774,
+          "rotation": 0,
+          "target": "0-shibuya--outside"
+        },
+        {
+          "yaw": -3.094733546361793,
+          "pitch": 0.4085402422350519,
+          "rotation": 0,
+          "target": "6-sky-tree-rayquaza"
+        },
+        {
+          "yaw": -1.2984554187342674,
+          "pitch": 0.24888042845301683,
+          "rotation": 0,
+          "target": "5-tokyo-dx-snorlax"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0328788620849387,
+          "pitch": 0.1985400034089082,
+          "title": "寶可夢牆",
+          "text": "東京DX店有好幾個入口，每個入口都有兩面牆，分別放上了每個世代的御三家"
+        }
+      ]
+    },
+    {
+      "id": "5-tokyo-dx-snorlax",
+      "name": "Tokyo Dx Snorlax",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.010685689297929457,
+        "pitch": -0.12466878938492698,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0707839761573954,
+          "pitch": 0.05118826390154929,
+          "rotation": 0,
+          "target": "4-tokyo-dx-entrance"
+        },
+        {
+          "yaw": 0.02003402461964754,
+          "pitch": 0.16008617016965232,
+          "rotation": 0,
+          "target": "6-sky-tree-rayquaza"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.33801195454592303,
+          "pitch": -0.27168687638450706,
+          "title": "卡比獸",
+          "text": "東京DX店的鎮店寶可夢是卡比獸，旁邊的裝飾是Let's Go 系列發售時為了慶祝擺上的"
+        }
+      ]
+    },
+    {
+      "id": "6-sky-tree-rayquaza",
+      "name": "Sky tree Rayquaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.10230061952933411,
+        "pitch": -0.0483409591492574,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.601740477192907,
+          "pitch": 0.3120968630186791,
+          "rotation": 0,
+          "target": "0-shibuya--outside"
+        },
+        {
+          "yaw": -2.8283256235019127,
+          "pitch": 0.3430756370543442,
+          "rotation": 0,
+          "target": "4-tokyo-dx-entrance"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5188295530545677,
+          "pitch": 0.07898932146269466,
+          "title": "列空座",
+          "text": "晴空塔店的鎮店寶可夢，做出了像是破開牆壁登場的感覺，相當氣派"
+        }
+      ]
+    }
+  ],
+  "name": "Tokyo",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
