91c6cc2e10a6adfbe8c28b330fa4f8e8a7ebd8fe
Ray3163 <hsieh.ray0215@gmail.com>
2024-11-08 10:27:10 +0800

    updated 11/8

---
+++ Home/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kyoto-ho-oh",
+      "name": "Kyoto Ho oh",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.1276277407458153,
+        "pitch": -0.07704242391849014,
+        "fov": 1.2595710900483308
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
