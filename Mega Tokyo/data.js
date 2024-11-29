33288bb496994b7a5d59eb71eef27605a9211435
Ray3163 <hsieh.ray0215@gmail.com>
2024-11-29 11:18:54 +0800

    2024/11/29 updated

---
+++ Mega Tokyo/data.js
@@ -0,0 +1,312 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-tokyo-mega-hallway",
+      "name": "Tokyo Mega hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.4872662198632334,
+        "pitch": 0.08408010637215568,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5529569123105205,
+          "pitch": 0.21830344875920105,
+          "rotation": 0,
+          "target": "1-tokyo-mega-en1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.6299827431655611,
+          "pitch": -0.09759122017636912,
+          "title": "寶可夢主題牆",
+          "text": "在寶可夢中心所在的地方總會有充滿寶可夢的主題牆，就像是在告訴經過的人們：這附近有寶可夢中心，快來找找吧!"
+        }
+      ]
+    },
+    {
+      "id": "1-tokyo-mega-en1",
+      "name": "Tokyo Mega en1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": -2.489017600213579,
+        "pitch": 0.1264531826598123,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.977237385911689,
+          "pitch": 0.3326930914869699,
+          "rotation": 5.497787143782138,
+          "target": "2-tokyo-mega-mewtwo"
+        },
+        {
+          "yaw": 2.220608756801667,
+          "pitch": 0.3489004336896997,
+          "rotation": 0,
+          "target": "3-tokyo-mega-en2"
+        },
+        {
+          "yaw": -0.42994473953051404,
+          "pitch": 0.5598700555783864,
+          "rotation": 0,
+          "target": "0-tokyo-mega-hallway"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.103783345611008,
+          "pitch": 0.015977432972494654,
+          "title": "Mega噴火龍Y",
+          "text": "Mega進化(又稱超級進化)是第六世代登場的特殊系統，它可以激發出寶可夢更強大的能力，這個模型便是Mega噴火龍的Y型態加上2018年的新春裝飾"
+        }
+      ]
+    },
+    {
+      "id": "2-tokyo-mega-mewtwo",
+      "name": "Tokyo Mega Mewtwo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -0.24933275028490343,
+        "pitch": 0.33621282826794996,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0263410932755939,
+          "pitch": 0.6647485347228557,
+          "rotation": 0,
+          "target": "4-tokyo-mega-inside"
+        },
+        {
+          "yaw": -1.8612919809863744,
+          "pitch": 0.4388749381183352,
+          "rotation": 0,
+          "target": "1-tokyo-mega-en1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.2459307981359906,
+          "pitch": 0.029531929468934237,
+          "title": "Mega超夢Y",
+          "text": "Mega超夢Y的身軀隨然變小了，但他的力量大到無法計算，即使只是稍稍使用念力也能讓一整棟大樓灰飛煙滅"
+        },
+        {
+          "yaw": -0.8920974506569941,
+          "pitch": 0.08173924972622437,
+          "title": "Mega路卡利歐",
+          "text": "Mega路卡利歐因為Mega能量和波導混和在一起在他的體內來回亂竄，讓他的身體多出了許多黑色的痕跡也讓他的鬥爭本能被覺醒了，在鬥爭本能的驅使下他會毫不留情地攻擊敵人，變得相當殘暴"
+        }
+      ]
+    },
+    {
+      "id": "3-tokyo-mega-en2",
+      "name": "Tokyo Mega en2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1415,
+      "initialViewParameters": {
+        "yaw": -3.0185842216584504,
+        "pitch": 0.2174960993716013,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4726065937874857,
+          "pitch": 0.4073643665822466,
+          "rotation": 0,
+          "target": "1-tokyo-mega-en1"
+        },
+        {
+          "yaw": 1.794256673571013,
+          "pitch": 0.4782505552296854,
+          "rotation": 0.7853981633974483,
+          "target": "5-tokyo-mega-charizard"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.954164783962465,
+          "pitch": -0.09886172694926643,
+          "title": "索爾迦雷歐&amp;露奈雅拉",
+          "text": "索爾迦雷歐和露奈雅拉是第七世代登場的寶可夢，他們兩個分別代表了日與月，是從異世界來的寶可夢"
+        }
+      ]
+    },
+    {
+      "id": "4-tokyo-mega-inside",
+      "name": "Tokyo Mega inside",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "yaw": -2.5149612000807764,
+        "pitch": 0.17415736228304013,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.416994372721245,
+          "pitch": 0.46332976923874014,
+          "rotation": 0,
+          "target": "5-tokyo-mega-charizard"
+        },
+        {
+          "yaw": 2.5439894258929305,
+          "pitch": 0.43746813466298917,
+          "rotation": 0,
+          "target": "2-tokyo-mega-mewtwo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-tokyo-mega-charizard",
+      "name": "Tokyo Mega Charizard",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.399103085772028,
+          "pitch": 0.48688573900868803,
+          "rotation": 0,
+          "target": "3-tokyo-mega-en2"
+        },
+        {
+          "yaw": 1.8357060280649868,
+          "pitch": 0.5022209323980729,
+          "rotation": 0.7853981633974483,
+          "target": "4-tokyo-mega-inside"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4640868965282223,
+          "pitch": -0.05107560236688968,
+          "title": "Mega噴火龍X",
+          "text": "噴火龍在進化成Mega噴火龍X的型態後外貌會有巨大的改變，除了身軀從原本的橘色和奶油色變成灰黑色與藍色外，他的火焰也從紅色變成了燃燒的更旺盛的藍色火焰"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
